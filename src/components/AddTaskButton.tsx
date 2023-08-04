64ae12fa584ba2e2cbdf2631917f00df6482299f
notamit <90555965+amitjimiwal@users.noreply.github.com>
2023-08-04 11:02:52 +0530

    upd

--- src/components/AddTaskButton.tsx
+++ src/components/AddTaskButton.tsx
@@ -5,6 +5,7 @@ interface Props{
 import React from "react";
 const AddTaskButton :React.FC<Props>= ({submit,toggle}) => {
   return (
+// {test case}
     <button className="bg-heading p-2 rounded-xl text-white" onClick={() => {submit()
       toggle()
     }}>Add Task</button>
